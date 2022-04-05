548e5a8581976642645d8caa881bca46205c61c2
Daníel Helgi Ágústsson <danielhelgi1@gmail.com>
2022-04-05 19:44:51 +0000

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.SphereGeometry(10, 10, 10);
+const material = new THREE.MeshBasicMaterial({ color: "yellow" });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+camera.position.z = 30;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.02;
+  sphere.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
